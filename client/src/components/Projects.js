c3a5bc5747cfeb636f1b0baa41580b33d513c51f
Xige Michael Chen <mxchen2001@gmail.com>
2021-06-26 05:00:41 -0500

    project cards

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
 
+import logo from '../logo.svg'
+
 function ProjectCards(props) {
     console.log('Hello')
     return (
-        <div class="max-w-sm rounded overflow-hidden shadow-lg">
-            <img class="w-full" class="lazy" data-src="/mountain.jpg" alt="Mountain"/>
+        <div class="cursor-pointer hover:bg-gray-100 max-w-sm rounded overflow-hidden shadow-lg">
+            <img class="w-full" class="lazy" src={logo} alt="Mountain"/>
             <div class="px-6 py-4">
-                <div class="font-bold text-xl mb-2">Mountain</div>
+                <div class="font-bold text-xl mb-2">{props.title}</div>
                 <p class="text-gray-700 text-base">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                    {props.text}
                 </p>
             </div>
             <div class="px-6 pt-4 pb-2">
-                {/* {props.tags.map((value, index) => {
+                {props.tags.map((value, index) => (
                     <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#{value}</span>
-                })} */}
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                )
+                )}
             </div>
         </div>
     );
@@ -32,7 +33,77 @@ const MyProjectCards = [
             'winter'
         ]
 
-    }
+    },
+    {
+        'title' : 'Lake',
+        'text' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.',
+        'tags' : [
+            'photography',
+            'travel',
+            'summer'
+        ]
+
+    },
+    {
+        'title' : 'Mountain',
+        'text' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.',
+        'tags' : [
+            'photography',
+            'travel',
+            'winter'
+        ]
+
+    },
+    {
+        'title' : 'Lake',
+        'text' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.',
+        'tags' : [
+            'photography',
+            'travel',
+            'summer'
+        ]
+
+    },
+    {
+        'title' : 'Mountain',
+        'text' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.',
+        'tags' : [
+            'photography',
+            'travel',
+            'winter'
+        ]
+
+    },
+    {
+        'title' : 'Lake',
+        'text' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.',
+        'tags' : [
+            'photography',
+            'travel',
+            'summer'
+        ]
+
+    },
+    {
+        'title' : 'Mountain',
+        'text' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.',
+        'tags' : [
+            'photography',
+            'travel',
+            'winter'
+        ]
+
+    },
+    {
+        'title' : 'Lake',
+        'text' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.',
+        'tags' : [
+            'photography',
+            'travel',
+            'summer'
+        ]
+
+    },
 ]
 
 class Projects extends Component {
@@ -44,10 +115,13 @@ class Projects extends Component {
         // console.log(MyProjectCards)
 
         return (
-            <div>
-                {MyProjectCards.map((el) => {
-                    ProjectCards()
-                })}
+            // <div class="flex flex-col justify-center items-center bg-gray-100">
+            <div class="pt-10 pb-10 pr-4 pl-4 grid xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+                {MyProjectCards.map((el) => (
+                    <div class="flex flex-col justify-center items-center">
+                        <ProjectCards title={el.title} text={el.text} tags={el.tags}/>
+                    </div>
+                ))}
             </div>
         );
     }
